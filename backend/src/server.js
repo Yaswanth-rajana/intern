7ada9cc12533f31ef5b38f3d672a202dcb91a57c
Yaswanth-rajana <yaswanthrajanaindiann@gmail.com>
2026-08-04 23:36:50 +0530

    backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ import { connectDB } from './config/database.js';
 // Load variables from .env
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Create HTTP server wrapping the Express app
 const server = http.createServer(app);
